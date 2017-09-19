8fe4ee4354bd0ddcecee3eeeb1340c7fe13ef608
Vinoth Kumar S <vinothshrsh@gmail.com>
2017-09-19 00:50:40 -0700

    Add files via upload

---
+++ js/demo.js
@@ -0,0 +1,12 @@
+﻿ 
+ 
+setTimeout(() => document.body.classList.add('render'), 60);
+
+const navdemos = document.querySelectorAll('nav.demos > .demo');
+
+    Array.from(navdemos).forEach(link => link.addEventListener('click', (ev) => {
+        ev.preventDefault();
+   
+    document.body.classList.remove('render');
+    document.body.addEventListener('transitionend', () => window.location = ev.target.href);
+}));
